eb855bca95c9745599305242ef8d17b9181abd26
d4lt <daniel_sena07@outlook.com>
2023-08-28 17:00:21 -0300

    initial commit

---
+++ src/components/todoItem.js
@@ -0,0 +1,21 @@
+import { View, Text, Pressable, Alert } from "react-native";
+import { todoStyle } from "../styles";
+
+export function Todo(props) {
+  return (
+    <View style={todoStyle.container}>
+      <Pressable
+        style={todoStyle.removeTodoBtn}
+        onPress={
+          () => props.setTodos(props.todos.filter( (t) => t.id !== props.todo.id ))
+        }
+      >
+        <Text style={{color: 'white', fontWeight: '700'}}>X</Text>
+      </Pressable>
+
+      <View style={todoStyle.todoTextContainer}>
+        <Text style={todoStyle.todoText}>{props.todo.todoName}</Text>
+      </View>
+    </View>
+  );
+}
